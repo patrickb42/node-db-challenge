1726debdea1b2fef868ea035e002d1e6c6da4b31
Patrick Baldwin <patrickbaldwin614@gmail.com>
2019-11-23 01:25:39 -0500

    restructured put ts and js in their own folders

--- js/data/dbConfig.js
+++ js/data/dbConfig.js
@@ -1,5 +1,5 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 var Knex = require("knex");
-var Config = require("../knexfile");
+var Config = require("../../knexfile");
 exports.default = Knex(Config.development);
