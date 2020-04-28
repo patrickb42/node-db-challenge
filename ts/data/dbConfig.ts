b267c1076b9cd9588dd634b921b42245f76acbf8
Patrick Baldwin <patrickbaldwin614@gmail.com>
2020-04-27 20:45:22 -0400

    added NODE_ENV to .env

--- ts/data/dbConfig.ts
+++ ts/data/dbConfig.ts
@@ -2,4 +2,4 @@ import * as Knex from 'knex';
 
 import * as Config from '../../knexfile';
 
-export default Knex(Config[process.env.NODE_ENV || 'development']);
+export default Knex(Config[process.env.NODE_ENV]);
